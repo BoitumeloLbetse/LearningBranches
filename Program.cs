8daeeddc82a8fe6053a9ee42f8f249ca582945bc
Boitumelo Lebetse <lebetseboitumelomelo@gmail.com>
2026-08-14 11:04:59 +0200

    second commit-pushing into the branch

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace TutorialCldv
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World");
-            Console.WriteLine("This is a C# console application");
+            Console.WriteLine("Lesedi is coming in to this world today !");
             Console.WriteLine("Running on .NET 10.0 with a C# 14.0 features");
         }
     }
