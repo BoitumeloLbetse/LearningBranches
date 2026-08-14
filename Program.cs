84ecd09bdcddf35226a183c4aefeef1b2d6353ed
Boitumelo Lebetse <lebetseboitumelomelo@gmail.com>
2026-08-14 10:45:04 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TutorialCldv
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("This is a C# console application");
+            Console.WriteLine("Running on .NET 10.0 with a C# 14.0 features");
+        }
+    }
+}
